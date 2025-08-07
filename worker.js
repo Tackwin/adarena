db704e55d2cda74210fea6aaa8e20101f0d1892f
Luca Bourroux <lucabrx@gmail.com>
2025-08-07 22:09:22 +0200

    Ennemy, new map, and two new tiles !

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "main_game";
-const CACHE_TIME = "2025-08-06T21:07:51.658Z";
+const CACHE_TIME = "2025-08-07T20:07:57.005Z";
 const CACHE_FULL = CACHE_NAME + "_" + CACHE_TIME;
 const CACHED_METADATA = ["", "index.html", "icon.png", "main.wasm", "manifest.json", "runtime.js"];
 
